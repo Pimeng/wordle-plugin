2d1750fd9a9bdfbc72c3ca6213be460641bfe3dd
Pimeng <i@pmya.xyz>
2025-09-23 21:50:10 +0800

    chore: 排版

--- utils/game.js
+++ utils/game.js
@@ -299,7 +299,7 @@ class WordleGame {
       const definition = await this.utils.word.getWordDefinition(gameData.targetWord);
       if (definition) {
         message += `
-【释义】：${definition}`;
+【释义】：\n${definition}`;
       }
       
       message += `\n你用了 ${gameData.attempts} 次猜测。\n成绩不错，再来一局吧！`;
@@ -310,7 +310,7 @@ class WordleGame {
       const definition = await this.utils.word.getWordDefinition(gameData.targetWord);
       if (definition) {
         message += `
-【释义】：${definition}`;
+【释义】：\n${definition}`;
       }
       message += `\n别灰心，再来一局吧！`;
       return message;
